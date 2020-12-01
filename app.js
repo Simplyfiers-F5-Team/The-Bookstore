e176572e608b3f8b8beebf9712271a9df9d6df0d
Paula <valdespaula@gmail.com>
2020-12-01 15:38:53 +0100

    Listado "simple" con handlebars

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-var express = require('express');
-var exphbs  = require('express-handlebars');
+var express = require('express'); //servidor web de node
+var exphbs  = require('express-handlebars'); //modulo de express para usar handlebars
  
-var app = express();
+var app = express(); //creamos objeto express en el servidor web
  
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
- 
-app.get('/', function (req, res) {
-    res.render('home');
+//los ficheros .handlebars los tratamos con handlebars, configurado para q los parciales estén en views
+app.engine('handlebars', exphbs({partialsDir: __dirname + '/views/partials/'})); 
+app.set('view engine', 'handlebars'); //los handlebars se renderizan a visualizar
+
+
+//home busca un main en layout
+app.get('/bookList', function (req, res) {                   //cada vez q se llame al razi del servidor, se renderice el home con esos datos
+    res.render('home', {books:["Libro1","Libro2"]});
 });
  
+app.get("/", function (req, res){
+    res.send("estás en el raiz de The Bookstore");
+}
+)
+
 app.listen(3000);
