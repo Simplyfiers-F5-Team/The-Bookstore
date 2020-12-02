ce075c8273d2dc8ab31c0edce1955f4ea7580237
imsuarezprieto <imsuarezprieto@outlook.com>
2020-12-02 21:10:26 +0100

    Fix passing two value to a rendering view.

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 var express = require('express'); //servidor web de node
-var exphbs  = require('express-handlebars'); //modulo de express para usar handlebars
+var exphbs = require('express-handlebars'); //modulo de express para usar handlebars
 
 
 const data = require('./data/books.json'); //Para cargar datos que vamos a cargar le pasamos el json con el objeto
 const cds = require("./data/cd.json");
 
 var app = express(); //creamos objeto express en el servidor web
- 
+
 //los ficheros .handlebars los tratamos con handlebars, configurado para q los parciales estén en views
-app.engine('handlebars', exphbs({partialsDir: __dirname + '/views/partials/'})); 
+app.engine('handlebars', exphbs({ partialsDir: __dirname + '/views/partials/' }));
 app.set('view engine', 'handlebars'); //los handlebars se renderizan a visualizar
 
 
 //home busca un main en layout
-app.get('/bookList', function (req, res) {                   //cada vez q se llame al razi del servidor, se renderice el home con esos datos
+app.get('/bookList', function(req, res) { //cada vez q se llame al razi del servidor, se renderice el home con esos datos
     res.render('home', data);
 });
- 
-app.get("/cdList", function (req, res){
-    res.render('home', {datos:{libros: data, cedes: cds}});
+
+app.get("/cdList", function(req, res) {
+    res.render('home', { libros: data, cds: cds });
 });
 
-app.listen(3000); 
+app.listen(3000);
