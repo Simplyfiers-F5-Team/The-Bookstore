ec71283da88642926083907a392c0aa7b8966022
imsuarezprieto <imsuarezprieto@outlook.com>
2020-12-03 19:49:53 +0100

    Scopped style with sass

--- helpers/style.js
+++ helpers/style.js
@@ -1,13 +1,20 @@
 const fs = require('fs');
 const path = require('path');
+const { v4: uuid } = require('uuid');
+const sass = require('sass');
 
 exports.style = function(fileName, options) {
-    const cssPath = path.join(__dirname, '..', 'views', 'partials', fileName);
+    const UUID = uuid();
+    const cssPath = path.join(__dirname, '..', 'views', 'partials', fileName + '.scss');
     const cssFile = fs.readFileSync(cssPath, (err, date) => {});
+    const scssStyle = `[data-uuid="${UUID}"]{` + cssFile + `}`;
+    const scss = sass.renderSync({ data: scssStyle });
     return `
 		<style>
-			${cssFile}
+			${scss.css.toString()}
 		</style>
+		<div data-uuid="${UUID}">
 		${options.fn(this)}
+		</div>
 	`;
 }
